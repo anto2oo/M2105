ddf311568efdb536dea9c05b5d9e240befbcb2e3
Antonin <antonin.guyot75@gmail.com>
2019-06-14 18:06:15 +0200

    small fixes

--- init-db.sql
+++ init-db.sql
@@ -2,21 +2,21 @@ DROP TABLE IF EXISTS CONTACT CASCADE;
 DROP TABLE IF EXISTS UTIL CASCADE;
 
 CREATE TABLE UTIL (
-	id_util serial primary key,
-	login varchar(20) not null,
-	mdp varchar(20) not null
+	id_util serial PRIMARY KEY,
+	login varchar(20) NOT NULL,
+	mdp varchar(20) NOT NULL
 );
 
 CREATE TABLE CONTACT (
-	id_contact serial primary key,
-	nom varchar(100) not null,
-	email varchar(100) not null,
-	tel varchar(10) not null,
-	adresse varchar(100) not null,
+	id_contact serial PRIMARY KEY,
+	nom varchar(100) NOT NULL,
+	email varchar(100) NOT NULL,
+	tel varchar(10) NOT NULL,
+	adresse varchar(100) NOT NULL,
 	latitude real,
 	longitude real,
-	id_util int references UTIL
+	id_util int REFERENCES UTIL
 );
 
-insert into UTIL(login, mdp) values ('root', 'root');
-insert into UTIL(login, mdp) values ('user', 'user');
+INSERT INTO UTIL(login, mdp) VALUES ('root', 'root');
+INSERT INTO UTIL(login, mdp) VALUES ('user', 'user');
